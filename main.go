0dddcb18082135db3d5720858c4ad9012fcd9a94
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-12-25 20:42:15 +0900

    feat: get api

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
-  fmt.Println("Hello, World")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World")
+	})
+	e.GET("/users/:name", getUserName)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUserName(c echo.Context) error {
+	name := c.Param("name")
+	return c.String(http.StatusOK, name)
 }
